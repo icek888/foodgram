26bc63dd52a60b92f9b8b2f9b190a25efd66c36a
Karlen <abelyankarlen@gmail.com>
2024-11-16 16:32:21 +0400

    testnew1

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,7 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',
+                          '89.169.168.101',
+                          'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
