64d7bb2cefa9622a576a70b83157dcb634911726
Karlen <abelyankarlen@gmail.com>
2024-11-17 18:48:33 +0400

    rlimit test3

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '89.169.168.101').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
